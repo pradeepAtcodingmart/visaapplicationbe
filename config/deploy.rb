28bc1513841fd23080d1706ad7c463a16db7faf2
pradeepAtcodingmart <pradeep@codingmart.com>
2019-01-23 12:36:30 +0530

    deploy.rb changed

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,47 +1,40 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
-
-set :application, "visaapplicationbe"
-set :repo_url, "https://github.com/pradeepAtcodingmart/visaapplicationbe.git"
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/var/www/visaapplicationbe"
-
-# Default value for :format is :airbrussh.
+set :application, 'visaapplicationbe' # application name
+set :deploy_to, '/var/www/visaapplicationbe'
+set :use_sudo, true
+# set :scm, :git
+set :keep_releases, 5
 set :format, :pretty
 set :log_level, :debug
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
 set :pty, true
-
-# Default value for :linked_files is []
-append :linked_files, "config/database.yml"
-
-# Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-set :keep_releases, 3
-
-set :stages, %w(development production testing)
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :stages, %w(production staging development)
 set :default_stage, "development"
-set :ssh_options, {:forward_agent => true,
-    :user=> "root",
-    :keepalive => true,
-    :keepalive_interval => 3000}
-set :user, "root"
+
+# Force rake through bundle exec
+SSHKit.config.command_map[:rake] = "bundle exec rake"
+
+# Force rails through bundle exec
+SSHKit.config.command_map[:rails] = "bundle exec rails"
+
 set :migration_role, 'app' # Defaults to 'db'
 set :assets_roles, [:app] # Defaults to [:web]
+
+# Shared directories over different deployments
+set :linked_files, %w(config/database.yml)
+
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      within release_path do
+        execute "sudo rm -rf tmp"
+        execute "sudo mkdir tmp"
+        execute "sudo chmod -R 777 #{current_path}/tmp"
+        execute "sudo chmod -R 777 #{shared_path}/tmp"
+        execute "sudo service nginx restart"
+      end
+    end
+  end
+
+  after :finishing, 'deploy:restart'
+end
